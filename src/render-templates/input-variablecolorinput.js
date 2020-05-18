a354f91c8603766ca21fcf62ef59c675fa66da07
amakazor <amakazor@gmail.com>
2020-05-18 15:10:33 +0200

    no padding in variable color input

--- src/render-templates/input-variablecolorinput.js
+++ src/render-templates/input-variablecolorinput.js
@@ -1,7 +1,7 @@
 const inputvariablecolorinput = `
 	<div class="custom-variable_input">
-	    <label class="switchVariableLabel col-sm-8" for="switchVariable{%=key%}">Use variable</label>
-	    <input class="col-sm-2 switchVariable" id="switchVariable{%=key%}" data-key="switchVariable" type="checkbox">
+	    <label class="switchVariableLabel col-sm-8 no-padding" for="switchVariable{%=key%}">Use variable</label>
+	    <input class="col-sm-2 switchVariable no-padding" id="switchVariable{%=key%}" data-key="switchVariable" type="checkbox">
 		<input class="nonVariableInput form-control" data-key="nonVariableInput" name="{%=key%}" type="color"
 			{% if (typeof value !== 'undefined') { %}value="{%=value%}"{% } %}
 		 	pattern="#[a-f0-9]{6}"/>
